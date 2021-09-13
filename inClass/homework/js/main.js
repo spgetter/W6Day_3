dd0b08309634d11282f16c109eeb411144513557
spgetter <spgetter@gmail.com>
2021-09-13 00:57:46 -0600

    W5Day_3HW commit

--- inClass/homework/js/main.js
+++ inClass/homework/js/main.js
@@ -1,27 +1,63 @@
+table_built = false;
+
 // Get our standings API data
 const getData = async (season, round) => {
-    console.log(`http://ergast.com/api/f1/${season}/${round}/driverstandings.json`)
-    let response = await axios.get(`http://ergast.com/api/f1/${season}/${round}/driverstandings.json`)
-    console.log(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
-    return response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+    console.log(`https://ergast.com/api/f1/${season}/${round}/driverstandings.json`)
+    let response = await axios.get(`https://ergast.com/api/f1/${season}/${round}/driverStandings.json`);
+    console.log(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    return response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+
 }
 
-const create_list = ( position, name, nationality, constructor, points) => {
-    const html = `<div class="standings_list_item">${position}</div><div class="standings_list_item">${name}</div><div class="standings_list_item">${nationality}</div><div class="standings_list_item">${constructor}</div><div class="standings_list_item">${points}</div>`;
-    document.querySelector(DOM_Elements.standings_list).insertAdjacentHTML('beforeend', html)
+const create_list = ( position, given_name, family_name, nationality, constructor, points) => {
+    if(position < 8){
+        let name = `${given_name} ${family_name}`;
+        const html = ` <div class="row standings_list">
+                        <div class="standings_list_item col-12 col-md-3 col-sm-3">${position}</div>
+                        <div class="standings_list_name col-12 col-md-2 col-sm-2">${name}</div>
+                        <div class="standings_list_item col-12 col-md-3 col-sm-3">${nationality}</div>
+                        <div class="standings_list_item col-12 col-md-2 col-sm-2">${constructor}</div>
+                        <div class="standings_list_item col-12 col-md-2 col-sm-2">${points}</div>
+                        </div>`;
+        document.querySelector(DOM_Elements.container).insertAdjacentHTML('beforeend', html);
+    }
+}
+
+
+const DOM_Elements = {
+    standings_list: '.standings_list',
+    standings_list_item: '.standings_list_item',
+    container: '.container'
 }
 
 // Function to Load Data and Displays HTML
 
-const load_data = async () => {
-    let query_season = document.querySelector('#season')
-    let query_round = document.querySelector('#round')
-    const drivers = await getData(query_season.value,query_round.value);
-    
-    drivers.forEach( element => create_list( element.position, element.Driver.driverID, element.Driver.nationality, element.Constructors[0].constructorId, element.points ))
+const form = document.querySelector('#testDataForm')
+form.addEventListener('submit', ( event ) => {
+    event.preventDefault();
+    let query_season = document.querySelector('#season');
+    let query_round = document.querySelector('#round');
+    if(table_built){
+        clear_data();
+    }
+    const html = ` <div class="row">
+                    <div class="tableHead col-12 col-md-3 col-sm-3">Position</div>
+                    <div class="tableHead standings_list_name col-12 col-md-2 col-sm-2">Name</div>
+                    <div class="tableHead col-12 col-md-3 col-sm-3">Nationality</div>
+                    <div class="tableHead col-12 col-md-2 col-sm-2">Constructor</div>
+                    <div class="tableHead col-12 col-md-2 col-sm-2">Points</div>
+                    </div>`;
+    document.querySelector(DOM_Elements.container).insertAdjacentHTML('beforeend', html);
+    load_data(query_season.value, query_round.value);
+});
+
+const load_data = async (season, round) => {
+    const drivers = await getData(season,round);
+    drivers.forEach( element => create_list( element.position, element.Driver.givenName, element.Driver.familyName, element.Driver.nationality, element.Constructors[0].name, element.points ));
+    table_built = true;
 }
 
 // Function to Clear Data from HTML
 const clear_data = () => {
-    document.querySelector(DOM_Elements.standings_list).innerHTML = "";
+    document.querySelector(DOM_Elements.container).innerHTML = "";
 }
